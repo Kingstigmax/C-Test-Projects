995148a6f260a2e954237ff8bbb09c3a428c8f28
parag0n <parag0n@ultron>
2024-10-02 14:11:58 -0500

    Lib-C

---
+++ Lib-C/lib.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>>
+
+
+typedef struct Book 
+{
+    int id;
+    char title[100];
+    char author[50];
+    int available;
+} Book;
+
+
+
+
+
+
+int main()
+{
+
+    return 0;
+}
